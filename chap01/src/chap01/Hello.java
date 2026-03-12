78f265f59d8b352c5b2fad6544fb12f06df9ff6a
rlaehddud475 <6070kjh475@naver.com>
2026-03-12 11:10:02 +0900

    .

--- chap01/src/chap01/Hello.java
+++ chap01/src/chap01/Hello.java
@@ -4,9 +4,8 @@ import java.util.Random;
 
 public class Hello {
  public void main() {
-	 System.out.println("안녕"); /*화면에 문자 출력*/
-	System.out.println("안녕"+"!");
-	System.out.println("김동영");
+	System.out.println("안녕"); /*화면에 문자 출력*/
+	System.out.println("안녕"+"! ");
 	String hello = "안녕!";	
 	System.out.println(hello);
 
---
+++ chap01/src/chap01/NumberTypeDemo.java
@@ -0,0 +1,17 @@
+package chap01;
+
+public class NumberTypeDemo {
+
+	public static void main(String[] args) {
+	int mach, distance;
+	mach=340;
+	distance=mach*60*60;
+	System.out.println("소리가 1시간 동안 가는 거리 : "+distance+"m");
+	double radius,area;
+	radius=10.0;
+	area=radius*radius*3.14;
+	System.out.println("반지름이"+radius+"인 원의 넓이 : "+area);
+	
+	}
+
+}
