6a2bd9e99972539b3b9c5bdb8db9351d37b414ad
rlaehddud475 <6070kjh475@naver.com>
2026-03-19 09:52:33 +0900

    .

---
+++ chap01/src/chap01/VarDemo.java
@@ -0,0 +1,17 @@
+package chap01;
+
+public class VarDemo {
+//	var a=1;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 
+		 int var =1;
+		 
+		 var x=1;
+//		 var x=1,y=3,z=4;
+//		 var str =null;
+//		var oops;
+//		oops=1;
+	}
+//void test(var x) {}
+}
