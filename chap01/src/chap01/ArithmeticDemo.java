44f9e7dd05fc17164df3bebcb8c224031392b88b
rlaehddud475 <6070kjh475@naver.com>
2026-03-26 09:25:26 +0900

    .

---
+++ chap01/src/chap01/ArithmeticDemo.java
@@ -0,0 +1,10 @@
+package chap01;
+
+public class ArithmeticDemo {
+
+	public static void main(String[] args) {
+		int remainder = 25%2;
+		System.out.println("25 ÷ 2의 나머지는 " + remainder + "입니다.");
+	}
+
+}
